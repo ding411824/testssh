1495a5e7bdfc0e7c3907f4177126bfe1a7c6a791
ding <ding@gmial.com>
2024-03-11 20:59:18 +0800

    ssh测试

---
+++ src/main/java/com/atguigu/TestShh.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+public class TestShh {
+    public static void main(String[] args) {
+        System.out.println("ssh......");
+        System.out.println("ssh......");
+        System.out.println("ssh......");
+        System.out.println("ssh......");
+    }
+}
